c06bd267cd873dfbbd4b0144a21c6166c655e3d0
Jakob Tottrup <jakobtottrup@gmail.com>
2020-04-27 15:19:04 +0200

    commit2

--- Task1/app.js
+++ Task1/app.js
@@ -1,17 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mainRouter = require('./routes/main');
+const usersRouter = require('./routes/users');
+const errorRouter = require('./routes/errors');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+
+/*app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir: `${__dirname}/views/layouts`,
+  partialsDir: `${__dirname}/views/partials`
+}));*/
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.set('view engine', 'hbs');
+app.use(express.static(`${__dirname}/public`));
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,10 +31,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', mainRouter);
+app.use('/', errorRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
+/*
 app.use(function(req, res, next) {
   next(createError(404));
 });
@@ -37,5 +51,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+*/
 
 module.exports = app;
