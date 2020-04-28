a7c356ced5f4af72f2682af4f512d23af8ff69e6
jakobtottrup <jakobtottrup@gmail.com>
2020-04-28 10:17:05 +0200

    fixed error

--- Task1/routes/main.js
+++ Task1/routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/main', { title: 'Home', layout: 'main' });
+  res.render('pages/main', { title: 'Home', layout: 'layouts/main' });
 });
 
 module.exports = router;
